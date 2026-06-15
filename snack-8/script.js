7910b18ba3c477cd29a8bcbf404eae721974301c
daviderocco85 <roccodavide85@gmail.com>
2026-06-15 18:37:19 +0200

    feat: add exercise solution for snack-8

---
+++ snack-8/script.js
@@ -0,0 +1,27 @@
+const students = [
+  {
+    id: 1,
+    name: 'Marco Lanci',
+    age: 32,
+    class: '3C'
+  },
+  {
+    id: 2,
+    name: 'Mario Banfi',
+    age: 34,
+    class: '4A'
+  },
+  {
+    id: 3,
+    name: 'Luigi Banzi',
+    age: 33,
+    class: '5B'
+  },
+];
+
+// Recupera la classe dello studente 'Marco Lanci'
+
+const getClassMarcoLanci = students.find((element)=> element.name.toLowerCase() === 'marco lanci'.toLowerCase());
+console.log(getClassMarcoLanci.class);
+
+// Risultato: '3C'
