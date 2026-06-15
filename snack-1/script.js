9bffdc7da342fda83f92ef4dcde653362bec3d7e
daviderocco85 <roccodavide85@gmail.com>
2026-06-15 18:28:12 +0200

    feat: add exercise solution for snack-1

---
+++ snack-1/script.js
@@ -0,0 +1,8 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+
+// Stampa in console tutti i nomi
+
+names.forEach((element)=> console.log(`nomi: ${element}`));
+
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
